6e0ad45bd5826743bd94ceec80b12a1100e78ee1
James Prince <james.prince1@gmail.com>
2023-01-25 12:09:51 +1100

    Added new startup development script. Added basic page container to further allow for further scaffolding

---
+++ client/src/pages/page-container.js
@@ -0,0 +1,24 @@
+import React, {useState} from "react";
+
+// TODO: Import state context
+
+
+// TODO: import other components
+
+export default function PageContainer() {
+    const [currentPage, setCurrentPage] = useState("Home");
+    return (
+        // currently using fragments, this can be made into a DOM element if needed for an overall wrapper
+        <>
+            <header>
+                
+            </header>
+            <main>
+
+            </main>
+            <footer>
+
+            </footer>
+        </>
+    )
+}
