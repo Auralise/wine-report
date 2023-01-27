2d5cff298dd3040a84eb104b93a9c6e3181d396b
James Prince <james.prince1@gmail.com>
2023-01-27 13:16:13 +1100

    Added an auth service class definition and export. I have disabled the eslint warning for this line but have also written an instantiation of the class with corresponding export left as comments which I will consult with others

---
+++ client/src/utils/auth.js
@@ -0,0 +1,44 @@
+import decode from "jwt-decode";
+
+class AuthService {
+
+    getToken() {
+        return localStorage.getItem("id_token");
+    }
+
+    isTokenExpired(token) {
+        const decoded = decode(token);
+
+        if (decoded.exp < Date.now() / 1000) {
+            localStorage.removeItem("id_token");
+            return true;
+        }
+
+        return false;
+    }
+
+    getProfile() {
+        return decode(this.getToken());
+    }
+
+    loggedIn() {
+        const token = this.getToken();
+        return token && !this.isTokenExpired(token) ? true : false;
+    }
+
+    login(idToken) {
+        localStorage.setItem("id_token", idToken);
+        window.location.assign("/");
+    }
+
+    logout() {
+        localStorage.removeItem("id_token");
+        window.location.reload();
+    }
+}
+
+// const AuthProvider = new AuthService();
+// export default AuthProvider;
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default new AuthService();
