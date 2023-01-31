82b40e2b086ab09944fc224e64217bcb83ceb471
James Prince <james.prince1@gmail.com>
2023-01-31 12:40:15 +1100

    Added producer/Winery model

--- server/models/producer.js
+++ server/models/producer.js
@@ -1,1 +1,25 @@
 import { Schema, model } from "mongoose";
+
+const producerSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: [2, "Please provide a winery name longer than 2 characters"],
+        maxlength: [50, "Please provide a winery name shorter than 50 characters"],
+    },
+    email: {
+        type: String,
+        match: [/.+@.+\..+/, "Must match an email address"],
+        required: false,
+    },
+    phone: {
+        type: String,
+        match: [/^\+?[0-9]{6,12}]/, "Please enter a phone number optionally starting with a + for international"],
+        required: false,
+    }
+});
+
+const Producer = model("Producer", producerSchema);
+
+export default Producer;
