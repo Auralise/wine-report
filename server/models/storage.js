47277cbedeb6ae1839ab20babe5bba2f7720d8d4
James Prince <james.prince1@gmail.com>
2023-01-31 16:42:48 +1100

    Added storage model - This will likely need to be discussed with lecturers to clarify as well as be fairly extenstively tested.

--- server/models/storage.js
+++ server/models/storage.js
@@ -1,1 +1,65 @@
 import { Schema, model } from "mongoose";
+
+const shelfSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 200,
+        
+    },
+    description: {
+        type: String,
+        required: false,
+    }
+});
+
+const storageUnitSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 200,
+        
+    },
+    description: {
+        type: String,
+        required: false,
+    },
+    shelves: [shelfSchema],
+});
+
+const storageRoomSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 200,
+        
+    },
+    description: {
+        type: String,
+        required: false,
+    },
+    units: [storageUnitSchema],
+});
+
+const storageFacilitySchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 200,
+        
+    },
+    description: {
+        type: String,
+        required: false,
+    },
+    rooms: [storageRoomSchema],
+});
+
+
+const StorageLocation = model("StorageLocation", storageFacilitySchema);
+
+export default StorageLocation;
