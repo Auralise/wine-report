03e87cbe9d80a3f0f24c5fa406da15b9a5ec27f4
James Prince <james.prince1@gmail.com>
2023-01-31 11:19:21 +1100

    Added region model

--- server/models/Region.js
+++ server/models/Region.js
@@ -1,1 +1,22 @@
 import { Schema, model } from "mongoose";
+
+const regionSchema = new Schema({
+    regionName: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: [2, "Please provide a region name longer than two characters"],
+        maxlength: [40, "Please shorten the region name to below 40 characters"]
+    },
+    countryName: {
+        type: String,
+        required: true,
+        default: "Australia",
+        minlength: [2, "Please provide a country name longer than two characters"],
+        maxlength: [40, "Please shorten the country name to below 40 characters"]
+    }
+});
+
+const Region = model("Region", regionSchema);
+
+export default Region;
