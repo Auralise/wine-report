dcde5dd55183fba217eef186ecaddebc6e0b7c00
James Prince <james.prince1@gmail.com>
2023-02-01 15:59:19 +1100

    Added wine search resolver and created a specific wine scaffold

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -46,12 +46,73 @@ const resolvers = {
             throw new GraphQLError("Please login", { extensions: { code: "UNAUTHORISED" } });
         },
 
-        producers: async (parent, {id, name}, context) => {
+        producers: async (parent, { id, name }, context) => {
             if (context.user) {
-                return id ? [Producer.findById(id)]: Producer.find({name});
+                return id ? [Producer.findById(id)] : Producer.find({ name });
             }
             throw new GraphQLError("Please login", { extensions: { code: "UNAUTHORISED" } });
+        },
+
+
+        // Wine search - Type determines any fixed category 
+        // Further development required to expand the search to handle multiple specific types
+        wine: async (parent, { searchTerm, type }, context) => {
+            if (context.user) {
+                const query = new RegExp(searchTerm, i)
+                switch (type) {
+                    case "Region":
+                        return Wine.find({
+                            region: {$regex: query},
+                        })
+                    case "Producer":
+                        return Wine.find({
+                            producer: {$regex: query},
+                        })
+                    case "Vintage":
+                        return Wine.find({
+                            vintage: {$regex: query},
+                        })
+                    case "Category":
+                        return Wine.find({
+                            category: {$regex: query},
+                        })
+                    case "Variety":
+                        return Wine.find({
+                            variety: {$regex: query},
+                        })
+                    case "Location":
+                        return Wine.find({
+                            location: {$regex: query},
+                        })
+                    default:
+                        return Wine.find({
+                            $or: [
+                                { name: { $regex: query } },
+                                { vintage: { $regex: query } },
+                                { variety: { $regex: query } },
+                                { region: { $regex: query } },
+                                { producer: { $regex: query } },
+                                { location: { $regex: query } },
+                                { category: { $regex: query } }
+                            ]
+                        });
+                }
+            }
+
+            throw new GraphQLError("Please login", { extensions: { code: "UNAUTHORISED" } });
+
+        },
+
+        specificWine: async (parent, {id}, context) => {
+            if (context.user) {
+            
+            }
+            
+            throw new GraphQLError("Please login", { extensions: { code: "UNAUTHORISED" } });
+
         }
+
+
     },
 
     Mutation: {
