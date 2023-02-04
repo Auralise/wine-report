ceba1420858fc52a1f7e715f602e366e747b46cd
James Prince <james.prince1@gmail.com>
2023-02-04 11:58:41 +1100

    Added gql queries for required routes.

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,19 +1,248 @@
-import { gql, useQuery } from "@apollo/client";
+import { gql } from "@apollo/client";
 
-// TODO: Define proper structure for this once database complexity has been worked out.
-// This has been defined as a placeholder for the moment
-export const GET_SINGLE_WINE = gql`
-    query getSingleWine($id: ID, $name: String, $vintage: Int, $winery: String){
-        wine {
+//For user management in further development
+export const GET_ALL_USERS = gql`
+    query getAllUsers {
+      User {
+        _id
+        name
+        email
+        role
+        approved
+      }
+    }
+`;
+
+export const GET_SPECIFIC_USER = gql`
+    query getSpecificUser ($email: String) {
+      User {
+        _id
+        name
+        email
+        role
+        approved
+      }
+    }
+`;
+
+export const GET_CURRENT_USER = gql`
+    query get
+`;
+
+export const GET_ALL_PRODUCERS = gql`
+    query getAllProducers {
+      producers {
+        _id
+        name
+        email
+        phone
+      }
+    }
+`;
+
+// Pass either parameter to find specific producer
+export const GET_SPECIFIC_PRODUCER = gql`
+    query getSpecificProducer ($id: ID, $name: String) {
+      producers {
+        _id
+        name
+        email
+        phone
+      }
+    }
+`;
+
+
+export const GET_ALL_REGIONS = gql`
+    query getAllRegions {
+      regions {
+        _id
+        name
+        country
+      }
+    }
+`;
+
+export const GET_SPECIFIC_REGION = gql`
+    query getSpecificRegion ($id: ID, $name: String) {
+      regions {
+        _id
+        name
+        country
+      }
+    }
+`;
+
+export const GET_ALL_STORAGE_LOCATIONS = gql`
+    query getAllStorageLocations {
+      storage {
+        _id
+        locationName
+        locationRoom
+        description
+      }
+    }
+`;
+
+export const GET_SPECIFIC_STORAGE_LOCATION = gql`
+    query getSpecificStorageLocations ($id: ID, $locationName: String) {
+      storage {
+        _id
+        locationName
+        locationRoom
+        description
+      }
+    }
+`;
+
+export const GET_ALL_VARIETIES = gql`
+    query getAllVarieties {
+      varieties {
+        _id
+        name
+      }
+    }
+`;
+
+export const GET_SPECIFIC_VARIETIES = gql`
+    query getSpecificVarieties ($id: ID, $name: String) {
+      varieties {
+        _id
+        name
+      }
+    }
+`;
+
+export const GET_ALL_WINE = gql`
+    query getAllWine {
+      wine {
+        _id
+        name
+        vintage
+        variety {
+          _id
+          name
+        }
+        region {
+          _id
+          name
+          country
+        }
+        producer {
+          _id
+          name
+          email
+          phone
+        }
+        comments {
+          _id
+          content
+          author {
             _id
             name
-            vintage
-            winery
-            location {
-                Shelf
-            }
+          }
+          createdAt
         }
+        locationStorage {
+          location {
+            _id
+            locationName
+            locationRoom
+            description
+          }
+          quantity
+        }
+        category
+      }
     }
+`;
 
-
+export const SEARCH_WINE = gql`
+    query searchWine($type: String, $searchTerm: String) {
+      wine {
+        _id
+        name
+        vintage
+        variety {
+          _id
+          name
+        }
+        region {
+          _id
+          name
+          country
+        }
+        producer {
+          _id
+          name
+          email
+          phone
+        }
+        comments {
+          _id
+          content
+          author {
+            _id
+            name
+          }
+          createdAt
+        }
+        locationStorage {
+          location {
+            _id
+            locationName
+            locationRoom
+            description
+          }
+          quantity
+        }
+        category
+      }
+    }
+`;
+  
+  
+export const GET_SINGLE_WINE = gql`
+    query getSingleWine($id: ID){
+      wine {
+        _id
+        name
+        vintage
+        variety {
+          _id
+          name
+        }
+        region {
+          _id
+          name
+          country
+        }
+        producer {
+          _id
+          name
+          email
+          phone
+        }
+        comments {
+          _id
+          content
+          author {
+            _id
+            name
+          }
+          createdAt
+        }
+        locationStorage {
+          location {
+            _id
+            locationName
+            locationRoom
+            description
+          }
+          quantity
+        }
+        category
+      }
+    }
+    }
 `;
